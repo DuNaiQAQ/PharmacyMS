6922be585677cf70fe52a4697c7a6bd4f10b5402
DuNaiRipAim <ovrjun@outlook.com>
2022-12-13 21:25:56 +0800

    Commit

--- src/main/java/com/jsu/pharmacyms/service/Impl/UserServiceImpl.java
+++ src/main/java/com/jsu/pharmacyms/service/Impl/UserServiceImpl.java
@@ -1,13 +1,10 @@
 package com.jsu.pharmacyms.service.Impl;
 
 import com.jsu.pharmacyms.dao.UserDao;
-import com.jsu.pharmacyms.domain.DashBoardInfo;
 import com.jsu.pharmacyms.domain.User;
 import com.jsu.pharmacyms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -23,35 +20,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String username) {
         return userDao.getUser(username);
-    }
-
-    /**
-     * <p>创建账户</p>
-     * @param id 用户ID
-     * */
-    @Override
-    public void createAccount(int id) {
-        userDao.createAccount(id);
-    }
-
-    @Override
-    public void changePassword(String password, int id) {
-        userDao.changePassword(id,password);
-    }
-
-    @Override
-    public void changePassword_admin(String password, String name) {
-        userDao.changePassword_admin(name,password);
-    }
-
-    @Override
-    public void addNewAccount_NoAdmin(String id,String username) {
-        userDao.createAccount_NoAdmin(id,username);
-    }
-
-    @Override
-    public DashBoardInfo getDashbordInfo() {
-        return new DashBoardInfo(userDao.getHomeInfo_drug(), userDao.getHomeInfo_user());
     }
 
 }
